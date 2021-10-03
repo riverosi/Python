8e6932ac87f076a81c4224b37cc36642f6706de8
riverosi <55927672+riverosi@users.noreply.github.com>
2021-10-02 22:26:32 -0300

    Updates in check data

--- Serial_Python/openbci_serial.py
+++ Serial_Python/openbci_serial.py
@@ -1,7 +1,7 @@
 import sys
 import serial
 import time
-from struct import pack
+import struct
 import numpy as np
 
 #Configure buffers for serial
@@ -14,7 +14,7 @@ BUFFER_SERIAL_RX_SIZE = 512
 #Configure baudrate
 BAUD_RATE = 460800
 
-buffer_data = bytearray(33)
+expected_footer = bytes((0 , 0 , 0 , 0 , 0 , 0 , 192))
 
 def list_serial_ports():
     import serial.tools.list_ports
@@ -35,14 +35,14 @@ def list_serial_ports():
 #para convertir de complemento a2 a counts de datos para los 8 canales de eeg
 def array_convert(bytes_array):
     counts_channel = []
-    counts_channel.append(int.from_bytes(bytes_array[1:3], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[4:6], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[7:9], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[10:12], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[13:15], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[16:18], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[19:21], byteorder='big',signed=True))
-    counts_channel.append(int.from_bytes(bytes_array[22:24], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[2:4], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[5:7], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[8:10], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[11:13], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[14:16], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[17:19], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[20:22], byteorder='big',signed=True))
+    counts_channel.append(int.from_bytes(bytes_array[23:25], byteorder='big',signed=True))
     return counts_channel
     
 
@@ -54,8 +54,17 @@ def read_serial():
     print("App is runing!!!")
 
     while True:
-        buffer_data = SerialPort.read_until(terminator='0xc0' ,size= 33)
-        print(len(buffer_data), array_convert(buffer_data))
+        try:
+            buffer_data = SerialPort.read_until(terminator=expected_footer)
+            if len(buffer_data) == 33:
+                print(array_convert(buffer_data))
+                #print(array_convert(struct.unpack('>33B', buffer_data)))
+            SerialPort.flush()
+        except KeyboardInterrupt:
+            #Interrup serial data read whit keyboard interrupt crtl + c
+            SerialPort.close()
+            print("Keyboard Interrupt")
+            break
 
         
 if __name__ == "__main__":
